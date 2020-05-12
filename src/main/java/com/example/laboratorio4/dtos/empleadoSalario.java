702b570e42f7736665c602c42b7ddc1a3e56f0b8
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-05-11 20:39:22 -0500

    search

---
+++ src/main/java/com/example/laboratorio4/dtos/empleadoSalario.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio4.dtos;
+
+public interface empleadoSalario {
+    String getName();
+
+    String getLast();
+
+    String getFechainicio();
+
+    String getFechafin();
+
+    String getPuesto();
+}
--- src/main/java/com/example/laboratorio4/repository/DepartmentsRepository.java
+++ src/main/java/com/example/laboratorio4/repository/DepartmentsRepository.java
@@ -1,6 +1,7 @@
 package com.example.laboratorio4.repository;
 
 import com.example.laboratorio4.dtos.empleadoPorDepa;
+import com.example.laboratorio4.dtos.empleadoSalario;
 import com.example.laboratorio4.dtos.salarioPromDepa;
 import com.example.laboratorio4.entity.Departments;
 
@@ -28,4 +29,21 @@ public interface DepartmentsRepository extends JpaRepository<Departments,Integer
             "where department_id=? \n" +
             "order by e.salary DESC",nativeQuery=true)
     List<empleadoPorDepa> obtenerEmpPorDepa(int id);
+
+    @Query(value="SELECT e.first_name as 'name' ,e.last_name as 'last' ,  date_format(date(jh.start_date),'%d-%m-%Y') as 'fechainicio',date_format(date(jh.end_date),'%d-%m-%Y') as 'fechafin', j.job_title as 'puesto'\n" +
+            "FROM job_history jh\n" +
+            "left join employees e on e.employee_id=jh.employee_id\n" +
+            "left join jobs j on j.job_id=e.job_id\n" +
+            "where e.salary >=8000;",nativeQuery=true)
+    List<empleadoSalario> buscarMayorSalario();
+
+    @Query(value="SELECT e.first_name as 'name' ,e.last_name as 'last' ,  date_format(date(jh.start_date),'%d-%m-%Y') as 'fechainicio',date_format(date(jh.end_date),'%d-%m-%Y') as 'fechafin', j.job_title as 'puesto'\n" +
+            "FROM job_history jh\n" +
+            "left join employees e on e.employee_id=jh.employee_id\n" +
+            "left join jobs j on j.job_id=e.job_id\n" +
+            "where e.salary =?;",nativeQuery=true)
+    List<empleadoSalario> buscarPorSalario(int salary);
+
+
+
 }
